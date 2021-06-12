ecc812db96434ccee83d872953392d1c939db406
Adrián Bielsa <63594921+adrianbielsa1@users.noreply.github.com>
2021-06-11 23:56:40 -0300

    Added `Viewer`'s re-exports to file "library.rs"

--- source-code/library.rs
+++ source-code/library.rs
@@ -4,8 +4,12 @@ mod token;
 mod parser;
 mod statement;
 
+mod viewer;
+
 pub use lexer::lex;
 pub use token::Token;
 
 pub use parser::parse;
 pub use statement::Statement;
+
+pub use viewer::Viewer;
